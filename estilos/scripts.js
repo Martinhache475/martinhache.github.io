a47e72c12e3e67397a5bd8e4f3b1f59d4fecde0d
Martinhache475 <108502607+Martinhache475@users.noreply.github.com>
2022-07-28 03:58:15 -0300

    version

---
+++ estilos/scripts.js
@@ -0,0 +1,16 @@
+
+function alerta() { alert("Debe ingresar solo texto")};
+
+function choice() {alert("Puede elegir mas de una opcion")};
+
+function getSelectValue()
+        {
+            var selectedValue = document.getElementById("list").value;
+            console.log(selectedValue);
+            alert("Corrobore los datos")
+            
+        }
+        
+function send() {alert("Una vez que confirme, no podrá editar el fornulario")};
+
+function fallo() {alert("haga click en el icono")};
